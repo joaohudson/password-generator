f2440a5ce90560f10d206704fa3d76ab2e95d69c
João Hudson <joao.oliveira@projecti.com.br>
2022-04-04 15:14:50 -0300

    adicionando atalho para copia de senha

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 
 const generatorButton = document.getElementById('generatorButton');
 const passwordLabel = document.getElementById('passwordLabel');
+const infoDiv = document.getElementById('infoDiv');
 
 function generatePassword(size){
     const data = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&*,.';
@@ -16,9 +17,23 @@ function generatePassword(size){
     return pass;
 }
 
+function displayCopyInfoMessage(enable){
+    if(enable){
+        infoDiv.style.display = 'block';
+    }else{
+        infoDiv.style.display = 'none';
+    }
+}
+
 generatorButton.onclick = () => {
     const pass = generatePassword(26);
     passwordLabel.innerText = pass;
+    displayCopyInfoMessage(false);
+}
+
+passwordLabel.onclick = () => {
+    displayCopyInfoMessage(true);
+    navigator.clipboard.writeText(passwordLabel.innerText);
 }
 
 })();
